58278dffee7253eee41658b6c203bd6e94a1361f
tk_storm <70317720+995854654@users.noreply.github.com>
2024-09-08 14:00:17 +0800

    update code

--- services/chat.py
+++ services/chat.py
@@ -1,6 +1,7 @@
 from models.settings import Setting
 from utils.logger import LoguruLogger
 from sse_starlette import ServerSentEvent
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
 class ChatService:
@@ -10,9 +11,24 @@ class ChatService:
 
     async def chat_simple_qa(self, context):
         self.logger.info("开始聊天")
-
-        async for stream in self.setting.chat_model.astream_events(
-                input=context,
+        prompt = ChatPromptTemplate.from_messages([
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ])
+        executor = (
+                {
+                    "question": lambda x: x["question"],
+                    "chat_history": lambda x: x["chat_history"]
+                }
+                | prompt
+                | self.setting.chat_model
+        )
+        chat_history = []
+        async for stream in executor.astream_events(
+                input={
+                    "question": context,
+                    "chat_history": chat_history
+                },
                 version="v1"
         ):
             event = stream["event"]
